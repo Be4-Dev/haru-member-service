6b4e7610b323252d1fc2eb733632ab7c361e6348
m-dzn <mdzn.dev@gmail.com>
2023-05-09 22:19:42 +0900

    feat(application): Member 등록 성공 시 ID도 함께 반환하도록 수정

--- src/main/kotlin/com/haru/member/application/in/RegisterMemberUseCase.kt
+++ src/main/kotlin/com/haru/member/application/in/RegisterMemberUseCase.kt
@@ -6,19 +6,21 @@ interface RegisterMemberUseCase {
     
     fun register(command: Command): Result
     
-    data class Command(
+    data class Command( // Request DTO
         val nickname:   String,
         val email:      String,
         val password:   String,
     )
     
-    data class Result(
+    data class Result( // Response DTO
+        val id:         Long?,
         val nickname:   String,
         val email:      String,
     )
     
     companion object {
         fun from(member: Member) = Result(
+            id          = member.id?.value,
             nickname    = member.nickname,
             email       = member.email
         )
