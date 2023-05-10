0f1516d9b5ec63502a1f8f0fcd13e7d308906fb4
m-dzn <mdzn.dev@gmail.com>
2023-05-11 06:04:25 +0900

    feat(adapter): JPA nickname, email 중복 검사 로직 구현

---
+++ src/main/kotlin/com/haru/member/adapter/out/persistence/member/MemberReadAdapter.kt
@@ -0,0 +1,19 @@
+package com.haru.member.adapter.out.persistence.member
+
+import com.haru.member.application.out.ReadMemberPort
+import org.springframework.stereotype.Component
+
+@Component
+class MemberReadAdapter(
+    private val memberRepository: MemberRepository,
+): ReadMemberPort {
+    
+    override fun existsByNickname(nickname: String): Boolean {
+        return memberRepository.existsByNickname(nickname)
+    }
+    
+    override fun existsByEmail(email: String): Boolean {
+        return memberRepository.existsByEmail(email)
+    }
+    
+}
--- src/main/kotlin/com/haru/member/adapter/out/persistence/member/MemberRepository.kt
+++ src/main/kotlin/com/haru/member/adapter/out/persistence/member/MemberRepository.kt
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface MemberRepository : JpaRepository<MemberJpaEntity, Long> {
+    
+    fun existsByNickname(nickname: String): Boolean
+    
+    fun existsByEmail(email: String): Boolean
+    
 }
