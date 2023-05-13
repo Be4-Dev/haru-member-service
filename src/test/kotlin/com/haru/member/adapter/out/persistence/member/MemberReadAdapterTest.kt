a6b111c92aac42cdac6e2c5ca3703e5ac141f5f1
m-dzn <mdzn.dev@gmail.com>
2023-05-13 19:20:38 +0900

    test: MemberReadAdapter의 닉네임/이메일 중복 검사 로직 테스트

---
+++ src/test/kotlin/com/haru/member/adapter/out/persistence/member/MemberReadAdapterTest.kt
@@ -0,0 +1,58 @@
+package com.haru.member.adapter.out.persistence.member
+
+import com.haru.member.adapter.out.persistence.config.JpaAuditingConfig
+import com.haru.member.domain.MemberDummy
+import com.haru.member.test.RepositoryTest
+import com.haru.member.test.TestConstants
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.context.annotation.Import
+
+//@formatter:off
+@RepositoryTest
+@Import(value = [MemberReadAdapter::class, MemberPersistenceAdapter::class, JpaAuditingConfig::class])
+internal class MemberReadAdapterTest(
+    private val adapterUnderTest            : MemberReadAdapter,
+    private val memberPersistenceAdapter    : MemberPersistenceAdapter,
+) : BehaviorSpec({
+
+    Given("1건의 회원 레코드가 주어졌을 때") {
+        val member      = MemberDummy.create()
+        val createdBy   = TestConstants.createdBy
+
+        memberPersistenceAdapter.saveNew(member, createdBy)
+
+        When("일치하는 닉네임이 있으면") {
+            val isExist = adapterUnderTest.existsByNickname(member.nickname)
+
+            Then("true를 반환한다.") {
+                isExist shouldBe true
+            }
+        }
+
+        When("일치하는 닉네임이 없으면") {
+            val isExist = adapterUnderTest.existsByNickname(member.nickname + "*")
+
+            Then("false를 반환한다.") {
+                isExist shouldBe false
+            }
+        }
+
+        When("일치하는 이메일이 있으면") {
+            val isExist = adapterUnderTest.existsByEmail(member.email)
+
+            Then("true를 반환한다.") {
+                isExist shouldBe true
+            }
+        }
+
+        When("일치하는 이메일이 없으면") {
+            val isExist = adapterUnderTest.existsByEmail(member.email + "*")
+
+            Then("false를 반환한다.") {
+                isExist shouldBe false
+            }
+        }
+    }
+
+})
